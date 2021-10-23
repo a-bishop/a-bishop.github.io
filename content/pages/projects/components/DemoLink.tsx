c88e37c3dab491d649b8364e5b3549652fef20c0
a-bishop <andrew.bishop53@gmail.com>
2021-10-23 14:23:45 -0700

    add project

--- content/pages/projects/components/DemoLink.tsx
+++ content/pages/projects/components/DemoLink.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
-import { jsx, Link as TLink } from "theme-ui";
-import { Link } from "gatsby";
+import { jsx, Link as TLink } from 'theme-ui';
+import { Link } from 'gatsby';
 // import { useState } from "react";
 // import { FaLink } from "react-icons/fa";
 
@@ -19,25 +19,12 @@ const DemoLink = ({ href, title }: Props) => {
       sx={{
         fontSize: [2, 2, 3],
         color: `text`,
-        "&:hover": { 
-          color: "primary"
+        '&:hover': {
+          color: 'primary',
         },
       }}
     >
-        {/* <div
-          onMouseEnter={() => setShowLink(true)}
-          onMouseLeave={() => setShowLink(false)}
-        > */}
-      <div>
-          {title}
-        {/* {showLink && (
-          <FaLink
-          sx={{ ml: 2 }}
-          size="0.7em"
-          />
-          )} */}
-      </div>
-          {/* </div> */}
+      <div>{title}</div>
     </TLink>
   );
 };
